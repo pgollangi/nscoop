5efcbcd8e4aab674c5b0904e1eacfaa6f42c9715
Prasanna Kumar <prassu158@gmail.com>
2020-09-23 22:30:31 +0530

    Setup lint

--- index.js
+++ index.js
@@ -1,51 +1,49 @@
 #!/usr/bin/env node
 
-const { program } = require('commander');
+const { program } = require('commander')
 
-
-const { getInstaller } = require('./src/helper');
-
-program
-    .version('0.1.0')
-    .description('Install portable cross-platform executable binaries via NPM');
+const { getInstaller } = require('./src/helper')
 
 program
-    .command('install [repo]')
-    .description('Install binary from git repository')
-    .action(function (repo, options) {
-        var InstallerDef = getInstaller(repo)
-        var installer = new InstallerDef(repo, options)
-        installer.install();
-    }).on('-h, --help', function () {
-        console.log('');
-        console.log('Examples:');
-        console.log('');
-        console.log('  $ nscoop install pgollangi/netselect');
-    });
-
+  .version('0.1.0')
+  .description('Install portable cross-platform executable binaries via NPM')
 
 program
-    .command('uninstall <cmd>')
-    .alias('ex')
-    .description('execute the given remote cmd')
-    .option("-e, --exec_mode <mode>", "Which exec mode to use")
-    .action(function (cmd, options) {
-        console.log('exec "%s" using %s mode', cmd, options.exec_mode);
-    }).on('--help', function () {
-        console.log('');
-        console.log('Examples:');
-        console.log('');
-        console.log('  $ deploy exec sequential');
-        console.log('  $ deploy exec async');
-    });
+  .command('install [repo]')
+  .description('Install binary from git repository')
+  .action(function (repo, options) {
+    var InstallerDef = getInstaller(repo)
+    var installer = new InstallerDef(repo, options)
+    installer.install()
+  }).on('-h, --help', function () {
+    console.log('')
+    console.log('Examples:')
+    console.log('')
+    console.log('  $ nscoop install pgollangi/netselect')
+  })
+
+program
+  .command('uninstall <cmd>')
+  .alias('ex')
+  .description('execute the given remote cmd')
+  .option('-e, --exec_mode <mode>', 'Which exec mode to use')
+  .action(function (cmd, options) {
+    console.log('exec "%s" using %s mode', cmd, options.exec_mode)
+  }).on('--help', function () {
+    console.log('')
+    console.log('Examples:')
+    console.log('')
+    console.log('  $ deploy exec sequential')
+    console.log('  $ deploy exec async')
+  })
 
 // must be before .parse()
 program.on('--help', () => {
-    console.log('');
-    console.log('Example call:');
-    console.log('  $ custom-help --help');
-});
+  console.log('')
+  console.log('Example call:')
+  console.log('  $ custom-help --help')
+})
 
-program.parse(process.argv);
+program.parse(process.argv)
 
-if (!program.args.length) program.help();
+if (!program.args.length) program.help()
--- src/installer.js
+++ src/installer.js
@@ -1,29 +1,26 @@
 class Installer {
+  constructor (repo, options) {
+    this.repo = repo
+    this.options = options
+  }
 
-    constructor(repo, options) {
-        this.repo = repo;
-        this.options = options;
-    }
+  install () {
+    // githubInstaller.install()
+  }
 
-    install() {
-        githubInstaller.install()
+  getArchitecture () {
+    var platform = process.platform
+    if (platform.startsWith('win')) {
+      return 'windows-amd64'
     }
-
-    getArchitecture() {
-        var platform = process.platform;
-        if (platform.startsWith("win")) {
-            return "windows-amd64";
-        }
-        if (platform.includes("darwin")) {
-            return "mac-386";
-        }
-        if (process.arch.includes("64")) {
-            return "linux-amd64";
-        }
-        return "linux-386";
+    if (platform.includes('darwin')) {
+      return 'mac-386'
     }
+    if (process.arch.includes('64')) {
+      return 'linux-amd64'
+    }
+    return 'linux-386'
+  }
 }
 
-
-
-exports.Installer = Installer;
+exports.Installer = Installer
