fbebb8edc1085699d554e5147346a16c6e911383
Prasanna Kumar <prassu158@gmail.com>
2020-09-25 22:57:50 +0530

    Console installation information and download progress and

--- src/installers/github.js
+++ src/installers/github.js
@@ -1,39 +1,97 @@
 const os = require('os')
 const fs = require('fs')
+const path = require('path')
 const fetch = require('node-fetch')
-var uniqueFilename = require('unique-filename')
+const uniqueFilename = require('unique-filename')
+const progress = require('progress-stream')
+const cliProgress = require('cli-progress')
 
-const Installer = require('../installer').Installer
+const { Installer } = require('../installer')
 
 const { findBinary, getBinaryFromArchive } = require('../helper')
 
 class GithubInstaller extends Installer {
-  getRepoPath () {
-    return this.repo
+  canInstall (repo, options) {
+    return true
+  }
+
+  getRepoURL () {
+    return `https://github.com/${this.repo}`
+  }
+
+  getLatestReleaseURL () {
+    return `https://api.github.com/repos/${this.repo}/releases/latest`
   }
 
   install () {
-    var path = this.getRepoPath()
+    var repoURL = this.getRepoURL()
+    console.log(`Repository: ${repoURL}`)
+    var latestRelease = this.getLatestReleaseURL()
 
-    return fetch(`https://api.github.com/repos/${path}/releases/latest`).then(res => res.json())
-      .then(release => release.assets).then(assets => {
-        return findBinary(assets, a => a.name).browser_download_url
-      }).then(fetch).then(res => res.body).then(this.saveArchive).then(getBinaryFromArchive).then(this.createShim)
+    var archiveName
+    return fetch(latestRelease).then(res => {
+      const body = res.json()
+      if (res.ok) {
+        return body
+      }
+      if (res.status === 404) {
+        throw new Error('No releases found or unknow github repository.')
+      }
+      return body.then(e => {
+        throw new Error(e.message)
+      })
+    }).then(release => {
+      console.log(`Latest release: ${release.name} (tag: ${release.tag_name})`)
+      return release.assets
+    }).then(assets => {
+      const asset = findBinary(assets, a => a.name)
+      archiveName = asset.name
+      console.log(`Found suitable binary for your platform - ${archiveName}`)
+      return asset.browser_download_url
+    }).then(fetch).then(this.saveArchive).then(archive => {
+      console.log(`Extracting archive ${archiveName}`)
+      const binary = getBinaryFromArchive(archive)
+      return binary
+    }).then(this.createShim)
   }
 
   /**
    *
-   * @param {NodeJS.ReadableStream} responseStream
+   * @param {fetch.Response} responseStream
    * @param {string} output file path
    */
-  saveArchive (responseStream, output) {
+  saveArchive (res, output) {
     if (!output) {
       output = uniqueFilename(os.tmpdir())
     }
+
+    const contentLength = res.headers.get('content-length')
+
+    const bar = new cliProgress.SingleBar({
+      format: 'Downloading.. |  {bar} | {percentage}% | Speed: {speed}',
+      barCompleteChar: '\u2588',
+      barIncompleteChar: '\u2591',
+      hideCursor: true
+    })
+    bar.start(contentLength, 0, {
+      speed: 'N/A'
+    })
+
+    const progressStream = progress({ time: 100 })
+
+    progressStream.on('progress', function (progress) {
+      bar.update(progress.transferred)
+    })
+
+    progressStream.on('finish', function (progress) {
+      bar.stop()
+    })
+
     return new Promise((resolve, reject) => {
       var archive = fs.createWriteStream(output)
-      responseStream
+      res.body
         .on('error', reject)
+        .pipe(progressStream)
         .pipe(archive)
         .on('finish', () => {
           resolve(output)
